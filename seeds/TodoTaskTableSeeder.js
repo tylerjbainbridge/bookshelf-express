097d960013c3c0fc58d716e2be4916a33fcbfb5d
Tyler Bainbridge <tylerbainbridge@Tylers-MacBook-Pro.local>
2017-03-05 22:59:17 -0500

    constants in seed file

--- seeds/TodoTaskTableSeeder.js
+++ seeds/TodoTaskTableSeeder.js
@@ -4,11 +4,14 @@ const casual = require('casual');
 const Promise = require('bluebird');
 const random = require('lodash/random');
 
+const TODOS_AMT = 20;
+const TASKS_AMT = 60;
+
 exports.seed = (knex) => {
   return knex.raw('SET foreign_key_checks = 0;').then(() => {
     return knex('todos').truncate()
     .then(() => {
-      const todos = [...new Array(20)].map(() => ({
+      const todos = [...new Array(TODOS_AMT)].map(() => ({
         content: casual.short_description
       }));
       return knex('todos').insert(todos);
@@ -17,9 +20,9 @@ exports.seed = (knex) => {
       return knex('tasks').truncate()
     })
     .then(() => {
-      const tasks = [...new Array(60)].map(() => ({
+      const tasks = [...new Array(TASKS_AMT)].map(() => ({
         content: casual.word,
-        todo_id: random(1, 20)
+        todo_id: random(1, TODOS_AMT)
       }));
       return knex('tasks').insert(tasks);
     })
